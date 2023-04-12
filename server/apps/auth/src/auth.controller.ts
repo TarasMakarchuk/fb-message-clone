a6322be7d2688b16e17cb7dd05621e682946bcb9
Taras Makarchuk <2278452@gmail.com>
2023-04-12 20:58:42 +0300

    :bookmark: setup RMQ microservices

--- server/apps/auth/src/auth.controller.ts
+++ server/apps/auth/src/auth.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
+  @Get('user')
   getHello(): string {
     return this.authService.getHello();
+  }
+
+  @MessagePattern({ cmd: 'get-user' })
+  async getUser(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+
+    return { user: 'USER' };
   }
 }
