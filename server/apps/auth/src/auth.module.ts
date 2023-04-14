f04f8b17a46c5853484993f66da731b81732a618
Taras Makarchuk <2278452@gmail.com>
2023-04-14 21:07:12 +0300

    :white_check_mark: docker setup microservices and DB

--- server/apps/auth/src/auth.module.ts
+++ server/apps/auth/src/auth.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: './.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('POSTGRES_URI'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
