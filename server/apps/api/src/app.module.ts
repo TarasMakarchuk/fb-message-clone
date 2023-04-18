9e92eac6f3cb1d6d6082b4251990b9a7858288a5
Taras Makarchuk <2278452@gmail.com>
2023-04-18 21:41:25 +0300

    :truck: add presence service and necessary modules

--- server/apps/api/src/app.module.ts
+++ server/apps/api/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { AuthModule } from '../../auth/src/auth.module';
+import { PresenceModule } from '../../presence/src/presence.module';
+import { SharedModule } from '@app/shared';
 
 @Module({
   imports: [
@@ -10,6 +13,9 @@ import { ClientProxyFactory, Transport } from '@nestjs/microservices';
       isGlobal: true,
       envFilePath: './.env',
     }),
+    AuthModule,
+    PresenceModule,
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [
@@ -21,7 +27,26 @@ import { ClientProxyFactory, Transport } from '@nestjs/microservices';
         const PASSWORD = configService.get('RABBITMQ_PASS');
         const HOST = configService.get('RABBITMQ_HOST');
         const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
-
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+            queue: QUEUE,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: 'PRESENCE_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const USER = configService.get('RABBITMQ_USER');
+        const PASSWORD = configService.get('RABBITMQ_PASS');
+        const HOST = configService.get('RABBITMQ_HOST');
+        const QUEUE = configService.get('RABBITMQ_PRESENCE_QUEUE');
         return ClientProxyFactory.create({
           transport: Transport.RMQ,
           options: {
